b94dfb4b5201df3b2283e27d8a776dd482a453fc
MMuller16 <1604261@rgu.ac.uk>
2018-03-22 13:47:03 +0000

    Update myfirstserver.js

--- lab13/myfirstserver.js
+++ lab13/myfirstserver.js
@@ -1,7 +1,7 @@
 
 var http = require('http');
 
-http.createServer(function (req, req){]
+http.createServer(function (req, req){
 
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
